7d941637d7fbe74bfab923f28351bd3264c5e256
ashy0611 <ashleedasilva.internship@gmail.com>
2026-04-09 10:48:47 +0530

    Added new  admin thinker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,6 +15,7 @@ php artisan migrate --force
 php artisan db:seed --force
 php artisan config:cache
 php artisan storage:link
+php artisan tinker --execute="App\Models\User::where('email','ashy06tv@gmail.com')->update(['is_admin'=>1]);"
 
 echo "==> Starting nginx..."
 service nginx start
